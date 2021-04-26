64e226d1693368667240adcb0365682a7dffc287
jbrunyon@student.fullsail.edu <JohnathanBlake135!>
2021-04-26 13:07:21 -0400

    Added styling and the flipcard function.

---
+++ game.js
@@ -0,0 +1,5 @@
+function FlipCard(child)
+{
+    var item = document.getElementsByTagName("img").item(child);
+    item.setAttribute("src", "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg");
+}
