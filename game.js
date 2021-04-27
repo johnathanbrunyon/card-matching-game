eca3fde91d591978cdfb54683f48c0056dd4d4ae
jbrunyon@student.fullsail.edu <JohnathanBlake135!>
2021-04-26 21:53:43 -0400

    Fixes

--- game.js
+++ game.js
@@ -1,10 +1,25 @@
 var flippedTotal = 0;
 var started = false;
+var flippedVals =
+    [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+    ]
 
 const imgSrcs =
     [
         "https://images.unsplash.com/photo-1556103255-4443dbae8e5a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG9ncmFwaGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
-        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+        "https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGF3bnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        "https://www.nature.com/immersive/d41586-021-00095-y/assets/3TP4N718ac/2021-01-xx_jan-iom_tree-of-life_sh-1080x1440.jpeg",
+        "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg"
     ]
 
 function getRandomInteger(max) {
@@ -24,6 +39,8 @@ function Shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
 
+    HideAllCards();
+
     return array;
 }
 
@@ -42,7 +59,8 @@ function FlipCard(child) {
             {
                 item.setAttribute("src", imgSrcs[child]);
                 console.log(`Card ${child} has been flipped`);
-                flippedTotal += 1
+                flippedVals[child] = true;
+                flippedTotal += 1;
             } catch (e) {
                 console.log(e);
             }
@@ -51,6 +69,7 @@ function FlipCard(child) {
             item.setAttribute("src", "img/cardBack.jpg");
             console.log(`Card ${child} has been flipped back to hide the front.`);
             flippedTotal -= 1;
+            flippedVals[child] = false;
         }
     } else {
         console.log("Game must be started");
@@ -70,6 +89,7 @@ function HideAllCards() {
     for(i = 0; i < 9; i++)
     {
         document.getElementsByTagName("img").item(i).setAttribute("src", "img/cardBack.jpg");
+        flippedVals[i] = false;
     }
 }
 
